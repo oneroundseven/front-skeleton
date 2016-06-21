b20a4956c84a93382e167737957a3f36f8ff95ff
roundseven <oneroundseven@gmail.com>
2016-06-21 17:33:03 +0800

    add autoprefixer & webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
+            }
+        ]
+    },
+    postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+};
